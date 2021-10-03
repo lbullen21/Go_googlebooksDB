42cc9f6b8bac0fd527189544c12f6452b95920cd
Lauren Bullen <lbullen@healthline.com>
2021-10-03 14:26:30 -0700

    working on routes

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
+	"log"
+	"net/http"
+
+	"google.golang.org/api/books/v1"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
-func main() {
+const (
+	// Manage your books
+	BooksScope = "https://www.googleapis.com/auth/books"
+)
+
+func connectDB() {
 	fmt.Println("Go now running")
 
 	db, err := sql.Open("mysql", "root@tcp(127.0.0.1:3306)/go_googlebooks")
@@ -16,11 +27,62 @@ func main() {
 	}
 	defer db.Close()
 
-	create, err := db.Query("CREATE TABLE Publishers (Id INT PRIMARY KEY, Publishers VARCHAR(255), Title VARCHAR(255), Author VARCHAR(255))")
+	create, err := db.Query(`CREATE TABLE IF NOT EXISTS books (
+		id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+		book_title TEXT NOT NULL,
+		book_desc TEXT NOT NULL,
+		page_count INT (10) NOT NULL,
+		publisher_id INT (10) NOT NULL
+		)`)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	create, err = db.Query(`CREATE TABLE IF NOT EXISTS authors (
+		id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+		author_name TEXT NOT NULL
+	)`)
 
 	if err != nil {
 		panic(err.Error())
 	}
 
+	create, err = db.Query(`CREATE TABLE IF NOT EXISTS publishers (
+		id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+		book_title TEXT NOT NULL,
+		author_name TEXT NOT NULL,
+		publisher_id INT (10) NOT NULL
+		)`)
+	if err != nil {
+		panic(err.Error())
+	}
+
 	defer create.Close()
+}
+
+// func getBooks(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Println("getting books")
+// }
+
+func handleRequests() {
+	// creates a new instance of a mux router
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	//gets all books in db
+	myRouter.HandleFunc("/books", getBooks).Methods("GET")
+
+	//get individual book
+	myRouter.HandleFunc("/book{id}", getOneBook).Methods("GET")
+
+	// Running the Server
+	log.Fatal(http.ListenAndServe(":3000", myRouter))
+}
+
+func main() {
+	ctx := context.Background()
+	booksService, err := books.NewService(ctx)
+	if err != nil {
+		fmt.Println("something wrong with google books")
+	}
+	connectDB()
 }
